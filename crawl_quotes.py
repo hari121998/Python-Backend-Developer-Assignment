7999a67f121183ec429bde6aa3f655fd36806890
hari121998 <haricharanpunna123@gmail.com>
2022-07-31 12:02:39 +0530

    requested_htm_file

--- crawl_quotes.py
+++ crawl_quotes.py
@@ -1,5 +1,24 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get("http://quotes.toscrape.com/")
+def save_html_file(html,path):
+    with open(path,"wb") as writing_file:
+        writing_file.write(html)
 
-print(r)
+
+def get_html_file(path):
+    with open(path,"rb") as read_file:
+        return read_file.read()
+
+request_html_file = requests.get("http://quotes.toscrape.com/")
+
+save_html_file(request_html_file.content,"crawling.html")
+
+html_file = get_html_file("crawling.html")        
+
+
+
+print(html_file)
+
+
+
