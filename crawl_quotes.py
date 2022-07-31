373c74f50afd5822103f4409fcb9e53bf7a9ab0e
hari121998 <haricharanpunna123@gmail.com>
2022-07-31 14:26:11 +0530

    quotes scrapped

--- crawl_quotes.py
+++ crawl_quotes.py
@@ -10,6 +10,14 @@ def get_html_file(path):
     with open(path,"rb") as read_file:
         return read_file.read()
 
+def get_tag_list_text(html):
+    new_tag_list=[]
+    for each_tag in html:
+        new_tag_list.append(each_tag.text.strip())
+        
+    return new_tag_list
+
+
 request_html_file = requests.get("http://quotes.toscrape.com/")
 
 save_html_file(request_html_file.content,"crawling.html")
@@ -17,8 +25,29 @@ save_html_file(request_html_file.content,"crawling.html")
 html_file = get_html_file("crawling.html")        
 
 
+soup = BeautifulSoup(html_file,"html.parser")
+tag = soup.select_one("body > div > div:nth-child(2) > div.col-md-8")
 
-print(html_file)
+tag1 = tag.select('.quote')
+
+all_quotes = []
+
+for each_quote in tag1:
+
+    quotes = {}
+    quotes["quote"] = each_quote.select_one('span').text.strip()
+    quotes['author'] = each_quote.select_one('.author').text.strip()
+    tag_html_list = each_quote.select('div .tags a') 
+    quotes["tag"] = get_tag_list_text(tag_html_list)
+
+    all_quotes.append(quotes)
+
+print(all_quotes)
+    
+
+
+
+
 
 
 
