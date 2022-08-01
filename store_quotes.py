686d9e72e047ab8a380f9d3428fb5833d281d569
hari121998 <haricharanpunna123@gmail.com>
2022-08-01 22:54:28 +0530

    tables created

--- store_quotes.py
+++ store_quotes.py
@@ -1,5 +1,6 @@
 import json 
 import sqlite3
+from tkinter.font import names
 
 def get_json_data():
     with open('quotes.json','r') as read_file:
@@ -9,7 +10,63 @@ def get_json_data():
 def get_list_from_quotes_authors_obj(json_obj,obj_name):
     return json_obj[obj_name]
 
+def distinct_author_list(list_1):
+    name_list = []
+    new_list= []
+    for each_name in list_1:
+        if each_name['name'] not in name_list:
+            name_list.append(each_name['name'])
+            new_list.append(each_name)
+    return new_list,name_list
+
+def connect_sqlite_to_database():
+    return sqlite3.connect('quotes.db')
+
+def create_table(table,table_name):
+    connection = connect_sqlite_to_database()
+    cursor_obj = connection.cursor()
+    drop_sql = "DROP TABLE IF EXISTS {}"
+    cursor_obj.execute(drop_sql.format(table_name))
+    cursor_obj.execute('''PRAGMA foreign_keys = ON''')
+    cursor_obj.execute(table)
+    print("Table Created")
+    connection.close()
+
 quotes_authors_obj = get_json_data()
 quotes_list = get_list_from_quotes_authors_obj(quotes_authors_obj,"quotes")
 authors_list = get_list_from_quotes_authors_obj(quotes_authors_obj,"authors")
-print(len(authors_list))
+
+authors_new_list,author_names_list = distinct_author_list(authors_list)
+
+quotes_table='''
+        CREATE TABLE quotes(
+            id INTEGER NOT NULL PRIMARY KEY,
+            quote TEXT,
+            author_name VARCHAR(250),
+            author_id INTEGER,
+            FOREIGN KEY (author_id) REFERENCES authors(id) 
+            ON DELETE CASCADE
+            );
+            '''
+authors_table='''
+        CREATE TABLE authors(
+            id INTEGER NOT NULL PRIMARY KEY,
+            author_name VARCHAR(250),
+            born TEXT,
+            reference TEXT
+            );
+            '''
+tags_table = '''
+        CREATE TABLE tags(
+            id INTEGER,
+            tag VARCHAR(250),
+            quote_id INTEGER,
+            FOREIGN KEY (quote_id) REFERENCES quotes(id) 
+            ON DELETE CASCADE
+            );
+            '''
+
+
+create_table(authors_table,"authors")
+create_table(quotes_table,"quotes")
+create_table(tags_table,"tags")
