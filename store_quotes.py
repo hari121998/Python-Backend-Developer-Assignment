9c36de4256215cf4e7b856e5d45a3e558853ec8a
hari121998 <haricharanpunna123@gmail.com>
2022-08-04 16:41:07 +0530

    store_quotes_updated

--- store_quotes.py
+++ store_quotes.py
@@ -1,7 +1,6 @@
 import json 
 import sqlite3
 
-
 def get_json_data():
     with open('quotes.json','r') as read_file:
         obj_file = json.loads(read_file.read())
@@ -20,75 +19,38 @@ def distinct_author_names_list(list_1):
 def connect_sqlite_to_database():
     return sqlite3.connect('quotes.db')
 
-def create_table(table,table_name):
+def create_table_in_database(table_statement,table_name):
     connection = connect_sqlite_to_database()
     cursor_obj = connection.cursor()
     drop_sql = "DROP TABLE IF EXISTS {}"
     cursor_obj.execute(drop_sql.format(table_name))
     cursor_obj.execute('''PRAGMA foreign_keys = ON''')
-    cursor_obj.execute(table)
+    cursor_obj.execute(table_statement)
     connection.close()
 
-def inserting_data_into_table(insert_query):
-    connection  = connect_sqlite_to_database()
-    cursor_obj = connection.cursor()
-    cursor_obj.executescript(insert_query)
-    connection.commit()
-    connection.close()
-
-def get_and_insert_author(id,each_obj,insert_string):
-    name = each_obj['name']
-    born = each_obj['born']
-    reference = each_obj['reference']
-    
-    insert_data = insert_string.format(id,name,born,reference)
-    inserting_data_into_table(insert_data)
-
-def get_and_insert_quotes(id,each_obj,insert_string):
-    quote=each_obj['quote']
-    if id ==91:
-        quote= quote[:10]+quote[12:31]+quote[32:]
-        
-    author_name = each_obj['author']
-    no_of_tags = len(each_obj['tags'])
-
-    author_id = author_names_list.index(author_name)+1
-    insert_data = insert_string.format(id,quote,author_name,no_of_tags,author_id)
-
-    inserting_data_into_table(insert_data)
-
-def get_and_insert_tags(quote_count,insert_tag,tags_list):
-    for each_tag in tags_list:
-        string_format = insert_tag.format(each_tag,quote_count)
-        inserting_data_into_table(string_format)
-
-
-quotes_authors_obj = get_json_data()
-quotes_list = get_list_from_quotes_authors_obj(quotes_authors_obj,"quotes")
-authors_new_list = get_list_from_quotes_authors_obj(quotes_authors_obj,"authors")
-
-author_names_list = distinct_author_names_list(authors_new_list)
-
-quotes_table='''
+def create_and_get_quotes_table_statement():
+    return '''
         CREATE TABLE quotes(
             id INTEGER NOT NULL PRIMARY KEY,
             quote VARCHAR,
-            author_name VARCHAR(250),
-            no_of_tags INTEGER,
             author_id INTEGER,
             FOREIGN KEY (author_id) REFERENCES authors(id) 
             ON DELETE CASCADE
             );
-            '''
-authors_table='''
+            '''   
+
+def create_and_get_authors_table_statement():
+    return '''
         CREATE TABLE authors(
-            id INTEGER NOT NULL PRIMARY KEY,
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             author_name VARCHAR(250),
             born VARCHAR(200),
             reference VARCHAR(250)
             );
             '''
-tags_table = '''
+    
+def create_and_get_tags_table_statement():
+    return '''
         CREATE TABLE tags(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             tag_name VARCHAR(250),
@@ -98,51 +60,95 @@ tags_table = '''
             );
             '''
 
-create_table(authors_table,"authors")
-create_table(quotes_table,"quotes")
-create_table(tags_table,"tags")
-
-insert_author = '''
+def create_and_get_author_insert_statement():
+    return '''
     INSERT INTO 
-        authors(id,author_name,born,reference)
+        authors(author_name,born,reference)
     VALUES(
-         {},
         "{}",
         "{}",
         "{}"
     );
-'''
-insert_quotes = '''
+    '''
+def create_and_get_quotes_insert_statement():
+    return '''
     INSERT INTO 
-        quotes(id,quote,author_name,no_of_tags,author_id)
+        quotes(id,quote,author_id)
     VALUES(
         {},
        "{}",
-       "{}",
-        {},
         {}
     );
-'''
-insert_tags = '''
+    '''
+def create_and_get_tags_insert_statement():
+    return '''
     INSERT INTO 
         tags(tag_name,quote_id)
     VALUES(
     "{}",
      {}
     );
-'''
+    '''
 
-id_count = 0
-for each_item in authors_new_list:
-    id_count+=1
-    get_and_insert_author(id_count,each_item,insert_author)
+def inserting_data_into_table(insert_query):
+    connection  = connect_sqlite_to_database()
+    cursor_obj = connection.cursor()
+    cursor_obj.executescript(insert_query)
+    connection.commit()
+    connection.close()
+
+def get_and_insert_author_into_database(each_author_row,insert_string):
+    name = each_author_row['name']
+    born = each_author_row['born']
+    reference = each_author_row['reference']
     
+    insert_data = insert_string.format(name,born,reference)
+    inserting_data_into_table(insert_data)
 
+def get_and_insert_quotes_into_database(id,each_row_quotes,insert_string):
+    quote=each_row_quotes['quote'].strip('... ')
+    if id ==91:
+        quote= quote[:10]+quote[12:31]+quote[32:]
+        
+    author_name = each_row_quotes['author']
+    author_id = author_names_list.index(author_name)+1
+    quotes_insert_data = insert_string.format(id,quote,author_id)
+
+    inserting_data_into_table(quotes_insert_data)
+
+def get_and_insert_tags_into_database(quote_count,insert_tag,each_tag_list):
+    if len(each_tag_list)==0:
+        string_format = insert_tag.format('',quote_count)
+        inserting_data_into_table(string_format)
+    for each_tag in each_tag_list:
+        string_format = insert_tag.format(each_tag,quote_count)
+        inserting_data_into_table(string_format)
+
+quotes_authors_object = get_json_data()
+quotes_list = get_list_from_quotes_authors_obj(quotes_authors_object,"quotes")
+authors_new_list = get_list_from_quotes_authors_obj(quotes_authors_object,"authors")
+
+author_names_list = distinct_author_names_list(authors_new_list)
+
+quotes_create_statement = create_and_get_quotes_table_statement()
+authors_create_statement = create_and_get_authors_table_statement()
+tags_create_statement = create_and_get_tags_table_statement()
+
+create_table_in_database(authors_create_statement,"authors")
+create_table_in_database(quotes_create_statement,"quotes")
+create_table_in_database(tags_create_statement,"tags")
+
+author_insert_statement = create_and_get_author_insert_statement()
+quotes_insert_statement = create_and_get_quotes_insert_statement()
+tags_insert_statement = create_and_get_tags_insert_statement()
+
+for each_row_item in authors_new_list:
+    get_and_insert_author_into_database(each_row_item,author_insert_statement)
+    
 id_count=0
-for each_item in quotes_list:
+for each_row_item in quotes_list:
     id_count+=1
-    get_and_insert_quotes(id_count,each_item,insert_quotes)
-    get_and_insert_tags(id_count,insert_tags,each_item['tags'])
+    get_and_insert_quotes_into_database(id_count,each_row_item,quotes_insert_statement)
+    get_and_insert_tags_into_database(id_count,tags_insert_statement,each_row_item['tags'])
     
-
 print("Data created and Inserted into quotes.db")
